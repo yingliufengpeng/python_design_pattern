9845c9b219a7142f8e886c2447bc50070fb70787
yingliufengpeng <2906631584@qq.com>
2019-12-22 19:25:34 +0800

    第五章节代码整理完毕

---
+++ P5_Singleton_Pattern/__init__.py
@@ -0,0 +1,26 @@
+
+
+class Single(object):
+
+    instance = None
+
+    def __init__(self, name):
+        self.name = name
+
+    def __new__(cls, *args, **kwargs):
+
+        if cls.instance:
+            return cls.instance
+
+        obj = object.__new__(cls)
+
+        cls.instance = obj
+
+        return obj
+
+a = Single('w')
+
+b = Single('p')
+
+print(a)
+print(b)
